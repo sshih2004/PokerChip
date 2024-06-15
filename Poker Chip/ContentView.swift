15b9279c825e15f90c57cf8f79019b1d1784464a
Steven Shih <stevenshi>
2024-06-15 09:58:19 -0500

    Initial Commit

---
+++ Poker Chip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poker Chip
+//
+//  Created by Steven Shih on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poker Chip/Poker_ChipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_ChipApp.swift
+//  Poker Chip
+//
+//  Created by Steven Shih on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Poker_ChipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
